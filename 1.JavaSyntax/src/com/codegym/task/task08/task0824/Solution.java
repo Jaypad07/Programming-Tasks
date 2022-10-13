0f8519af674c6c352c6d71b577a99eba8194ce7f
Jaypad07 <jaypad07@gmail.com>
2022-10-12 18:07:07 -0700

    Make a Family Task - Updated

--- 1.JavaSyntax/src/com/codegym/task/task08/task0824/Solution.java
+++ 1.JavaSyntax/src/com/codegym/task/task08/task0824/Solution.java
@@ -6,46 +6,37 @@ Make a family
 */
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Solution {
     public static void main(String[] args) {
 
         ArrayList<Object> family = new ArrayList<>();
-        ArrayList<Human> children = new ArrayList<>();
-        ArrayList<Human> children1 = new ArrayList<>();
-        ArrayList<Human> children2 = new ArrayList<>();
-        ArrayList<Human> children3 = new ArrayList<>();
 
-        Human grandFather1 = new Human("Cherri", true, 65, children1);
-        Human grandFather2 = new Human("Camarino", true, 67, children);
+        Human child3 = new Human("Jay", true, 36);
+        Human child2 = new Human("Valerie", false, 39);
+        Human child1 = new Human("Erica", false, 42);
 
-        Human grandMother1 = new Human("Ana", false, 79, children1);
-        Human grandMother2 = new Human("Ketty", false, 89, children);
 
-        Human father = new Human("Jaime", true, 64, children2);
-        Human mother = new Human("Sandra", false, 63, children2);
+        Human father = new Human("Jaime", true, 64, child1, child2, child3);
+        Human mother = new Human("Sandra", false, 63, child1, child2, child3);
 
-        Human child3 = new Human("Jay", true, 36, children3);
-        Human child2 = new Human("Valerie", false, 39, children3);
-        Human child1 = new Human("Erica", false, 42, children3);
+        Human grandMother1 = new Human("Ana", false, 79, mother);
+        Human grandFather1 = new Human("Welito", true, 65, mother);
 
-        family.add(grandFather1);
+        Human grandMother2 = new Human("Ketty", false, 89, father);
+        Human grandFather2 = new Human("Camarino", true, 67, father);
+
         family.add(grandFather2);
-        family.add(grandMother1);
         family.add(grandMother2);
+        family.add(grandFather1);
+        family.add(grandMother1);
+
         family.add(father);
         family.add(mother);
         family.add(child1);
         family.add(child2);
         family.add(child3);
-
-        children.add(father);
-        children1.add(mother);
-
-        children2.add(child1);
-        children2.add(child2);
-        children2.add(child3);
-
 
         for (Object x : family) {
             System.out.println(x.toString());
@@ -57,13 +48,19 @@ public class Solution {
         public String name;
         public boolean sex;
         public int age;
-        public ArrayList<Human> children;
+        public ArrayList<Human> children = new ArrayList<>();
 
-        public Human(String name, boolean sex, int age, ArrayList<Human> children) {
+        public Human(String name, boolean sex, int age) {
             this.name = name;
             this.sex = sex;
             this.age = age;
-            this.children = children;
+        }
+
+        public Human(String name, boolean sex, int age, Human... children) {
+            this.name = name;
+            this.sex = sex;
+            this.age = age;
+            Collections.addAll(this.children, children);
         }
 
         public String toString() {
