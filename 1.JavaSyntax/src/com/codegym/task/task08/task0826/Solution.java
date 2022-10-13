4a4ccc805130c0fc812ac3bcccca28ecc787ef18
Jaypad07 <jaypad07@gmail.com>
2022-10-12 18:39:38 -0700

    Max Sort

---
+++ 1.JavaSyntax/src/com/codegym/task/task08/task0826/Solution.java
@@ -0,0 +1,41 @@
+package com.codegym.task.task08.task0826;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Five winners
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+
+        sort(array);
+
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+        System.out.println(array[3]);
+        System.out.println(array[4]);
+    }
+
+    public static void sort(int[] array) {
+        int temp;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i; j < array.length; j++) {
+                if (array[i] < array[j]) {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+
+        }
+    }
+}
