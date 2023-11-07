62b6090f106c3135d9562b1bf13844d349b5dc2a
Jaypad07 <jaypad07@gmail.com>
2023-11-07 11:30:01 -0700

    Writing my own ThreadFactory
    In the Solution class, create a public static AmigoThreadFactory class that implements the ThreadFactory interface.
    1. The implementation of the interface's method must create and return a thread that:
    1.1. is not a daemon,
    1.2. has a normal priority level,
    1.3. The thread's name must follow the pattern "GN-pool-A-thread-B" where: GN is the group name,
    A is the factory number, which is incremented inside the class, starting from 1 (use AtomicInteger),
    B is the thread number, which is incremented inside a specific factory, starting from 1 (use AtomicInteger).
    2. Each factory must contain the thread group (ThreadGroup) it was created in.
    3. The main and emulateThreadFactory methods are not tested.
    
    Example output:
    secondGroup-pool-2-thread-1
    firstGroup-pool-1-thread-1
    firstGroup-pool-1-thread-3
    secondGroup-pool-2-thread-3
    firstGroup-pool-1-thread-2
    secondGroup-pool-2-thread-2

---
+++ src/com/codegym/task/task28/task2802/Solution.java
@@ -0,0 +1,67 @@
+package com.codegym.task.task28.task2802;
+
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/* 
+Writing our own ThreadFactory
+
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        class EmulatorThreadFactoryTask implements Runnable {
+            @Override
+            public void run() {
+                emulateThreadFactory();
+            }
+        }
+
+        ThreadGroup group = new ThreadGroup("firstGroup");
+        Thread thread = new Thread(group, new EmulatorThreadFactoryTask());
+
+        ThreadGroup group2 = new ThreadGroup("secondGroup");
+        Thread thread2 = new Thread(group2, new EmulatorThreadFactoryTask());
+
+        thread.start();
+        thread2.start();
+    }
+
+    private static void emulateThreadFactory() {
+        AmigoThreadFactory factory = new AmigoThreadFactory();
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+            }
+        };
+        factory.newThread(r).start();
+        factory.newThread(r).start();
+        factory.newThread(r).start();
+    }
+
+    public static class AmigoThreadFactory implements ThreadFactory {
+        static final AtomicInteger poolNumber = new AtomicInteger(1);
+        final ThreadGroup group;
+        final AtomicInteger threadNumber = new AtomicInteger(1);
+        final String namePrefix;
+
+        AmigoThreadFactory() {
+            group = Thread.currentThread().getThreadGroup();
+            namePrefix = "-pool-" + poolNumber.getAndIncrement() + "-thread-";
+        }
+        @Override
+        public Thread newThread(Runnable r) {
+            Thread t = new Thread(group, r, group.getName() + namePrefix + threadNumber.getAndIncrement(), 0);
+            if (t.isDaemon()) {
+                t.setDaemon(false);
+            }
+            if (t.getPriority() != Thread.NORM_PRIORITY) {
+                t.setPriority(Thread.NORM_PRIORITY);
+            }
+            return t;
+        }
+    }
+}
